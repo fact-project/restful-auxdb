cb742a22794995c7a8643a30da4d985a05260645
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2016-06-19 21:10:30 +0200

    Add pages for services

--- auxservice/__init__.py
+++ auxservice/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_restful import Api
 from flask_json import FlaskJSON
+from .database import database
 
 from .resources import (
     MagicWeatherResource,
@@ -24,3 +25,20 @@ api.add_resource(DriveSourceResource, '/drive_source')
 @app.route('/')
 def main_page():
     return render_template('index.html')
+
+
+@app.route('/services')
+def services():
+    services = database.collection_names()
+    services.remove('system.indexes')
+    services.sort()
+    return render_template('services.html', services=services)
+
+
+@app.route('/services/<service>')
+def service(service):
+    entry = database[service].find_one()
+    fields = list(entry.keys())
+    fields.remove('_id')
+    fields.sort()
+    return render_template('service.html', fields=fields, service=service)
