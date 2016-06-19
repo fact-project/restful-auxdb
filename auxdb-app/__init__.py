5d288d99318ddb5abbc8c168f10114fc3cad6d5f
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2016-06-19 02:11:33 +0200

    Fix api

--- auxdb-app/__init__.py
+++ auxdb-app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_restful import Api
-from .magic_weather import MagicWeatherResource
+from flask_json import FlaskJSON
 
+from .magic_weather import MagicWeatherResource
 
 app = Flask('FACT auxdb rest interface')
 api = Api(app)
+json = FlaskJSON(app)
 
 api.add_resource(MagicWeatherResource, '/magic_weather')
--- auxdb-app/magic_weather.py
+++ auxdb-app/magic_weather.py
@@ -1,25 +1,19 @@
-from flask_restful import Resource, reqparse, marshal_with, fields
-from flask.json import jsonify
+from flask_restful import Resource, reqparse, inputs
 from .database import database
-from datetime import datetime
+from flask_json import as_json
 
 collection = database.magic_weather
 
 parser = reqparse.RequestParser()
 parser.add_argument('field', action='append')
-parser.add_argument('from', type=datetime)
-parser.add_argument('until', type=datetime)
-
-
-resource_fields = {
-    'timestamp': fields.DateTime(dt_format='iso8601')
-}
+parser.add_argument('from', type=inputs.datetime_from_iso8601)
+parser.add_argument('until', type=inputs.datetime_from_iso8601)
 
 
 class MagicWeatherResource(Resource):
+    @as_json
     def get(self):
         args = parser.parse_args(strict=True)
-        print(args['field'])
 
         query = {}
         projection = {'_id': False}
@@ -34,4 +28,4 @@ class MagicWeatherResource(Resource):
         if args['until']:
             query['timestamp'] = {'$lt': args['until']}
 
-        return jsonify(list(collection.find(query, projection=projection)))
+        return {'objects': collection.find(query, projection=projection)}
