03d631f2d699b27a3c601de4af5a93542808c26c
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2016-06-19 18:23:11 +0200

    Add more services

--- auxservice/__init__.py
+++ auxservice/__init__.py
@@ -2,10 +2,20 @@ from flask import Flask
 from flask_restful import Api
 from flask_json import FlaskJSON
 
-from .resources import MagicWeatherResource
+from .resources import (
+    MagicWeatherResource,
+    PfMiniResource,
+    DriveSourceResource,
+    DrivePointingResource,
+    DriveTrackingResource,
+)
 
 app = Flask('FACT auxdb rest interface')
 api = Api(app)
 json = FlaskJSON(app)
 
 api.add_resource(MagicWeatherResource, '/magic_weather')
+api.add_resource(PfMiniResource, '/pf_mini')
+api.add_resource(DriveTrackingResource, '/drive_tracking')
+api.add_resource(DrivePointingResource, '/drive_pointing')
+api.add_resource(DriveSourceResource, '/drive_source')
--- auxservice/resources.py
+++ auxservice/resources.py
@@ -1,11 +1,8 @@
 from flask_restful import Resource, reqparse
-from .database import database
+from .database import database as db
 from flask_json import as_json
 from .utils import convert_to_datetime
 from .utils import build_from_until_query
-
-
-collection = database.magic_weather
 
 parser = reqparse.RequestParser()
 parser.add_argument('field', action='append')
@@ -13,12 +10,13 @@ parser.add_argument('from', type=convert_to_datetime)
 parser.add_argument('until', type=convert_to_datetime)
 
 
-class MagicWeatherResource(Resource):
+class AuxResource(Resource):
+    collection = None
+
     @as_json
     def get(self):
         args = parser.parse_args(strict=True)
 
-        query = {}
         projection = {'_id': False}
 
         if args['field']:
@@ -27,4 +25,24 @@ class MagicWeatherResource(Resource):
 
         query = build_from_until_query(args['from'], args['until'])
 
-        return {'objects': collection.find(query, projection=projection)}
+        return {'objects': self.collection.find(query, projection=projection)}
+
+
+class MagicWeatherResource(AuxResource):
+    collection = db.magic_weather
+
+
+class PfMiniResource(AuxResource):
+    collection = db.pf_mini
+
+
+class DriveTrackingResource(AuxResource):
+    collection = db.drive_tracking
+
+
+class DrivePointingResource(AuxResource):
+    collection = db.drive_pointing
+
+
+class DriveSourceResource(AuxResource):
+    collection = db.drive_source
