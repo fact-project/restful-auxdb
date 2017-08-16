a49cb5711569458a4da366c061e82726bae8def9
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-08-16 11:24:22 +0200

    Display log

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 service nginx start
-uwsgi --ini /var/www/auxservice-www/auxservice_uwsgi.ini
+uwsgi --ini /var/www/auxservice-www/auxservice_uwsgi.ini &
+touch  /var/log/uwsgi/auxservice_uwsgi.log
+tail -f /var/log/uwsgi/auxservice_uwsgi.log
